37b12045f19ccd3d2a79f9724e8ffe8488b7a291
Nirajan Bohara <boharanirajan430@gmail.com>
2024-05-31 15:32:55 +0545

    completed

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_app/models/meal.dart';
 import 'package:meal_app/providers/favorites_provider.dart';
